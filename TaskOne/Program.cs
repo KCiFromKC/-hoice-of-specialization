c96d9d84104ed1231155b54d98c854248a50639d
Aleksandr_Kovtun <myworlditstruth@mail.ru>
2022-12-26 15:56:05 +0200

    add method FillArray

--- TaskOne/Program.cs
+++ TaskOne/Program.cs
@@ -1,2 +1,25 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Напишите программу, которая из имеющегося массива строк формирует массив из строк, длина которых меньше либо равна 3 символа. 
+// Первоначальный массив можно ввести с клавиатуры, либо задать на старте выполнения алгоритма.
+
+Console.Clear();
+
+Console.WriteLine("Введите желаемый размер массива:");
+int arrayLenght = Convert.ToInt32(Console.ReadLine());
+
+string[] array = new string[arrayLenght];
+
+FillArray(array);
+Console.WriteLine($"Получился следующий массив: {string.Join(", ",array)}");
+
+string[] FillArray(string[] array)
+{
+    int j = 1;
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.WriteLine($"Введите значение {j} элемента массива:");
+        array[i] = Console.ReadLine();
+        if (array[i] == "") array[i] = " ";
+        j++;
+    }
+    return array;
+}
